7fbc52affdbfc530d846b5b44873e9ae2768c662
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2022-12-15 16:24:01 +0100

    Add files via upload

---
+++ Jatek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labirintus
+{
+    public static class Jatek
+    {
+        public static void Teszt() {
+            Console.WriteLine("Teszt");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using static labirintus.Jatek;
+
+Teszt();
